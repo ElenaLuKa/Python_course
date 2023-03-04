4f167ed8405ba9b73a1738d29252cb9bc15ca02b
ElenaLu <you@example.com>
2023-03-04 10:04:23 +0300

    Added Seminar_02.py and corrected HW_02

--- Lesson_2/HW_02/Task 2_1.py
+++ Lesson_2/HW_02/Task 2_1.py
@@ -2,10 +2,18 @@
 # Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть
 
+# n = int(input())
+# count_reshka = 0
+# for i in range(n):
+#     v = int(input())
+#     if v == 0:
+#         count_reshka += 1
+# print("Переворачиваем монетки:", count_reshka if count_reshka < n / 2 else n - count_reshka)
+
+#или
 n = int(input())
-count_reshka = 0
+count = 0
 for i in range(n):
-    v = int(input())
-    if v == 0:
-        count_reshka += 1
-print("Переворачиваем монетки:", count_reshka if count_reshka < n / 2 else n - count_reshka)
+    if int(input()):
+        count += 1
+print(min(count, n - count))
